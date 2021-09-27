8da294b06cb2db9f06bf392ba18a7f1723648d48
Srikanth <srikanthh994@gmail.com>
2021-09-27 12:45:31 +0530

    Added:Welcome to SnakeLadder Position

---
+++ UC1_Startposition.java
@@ -0,0 +1,10 @@
+package bridgelabzs;
+
+	public class UC1_Startposition {
+		public static void main(String[] args) {
+			System.out.println("Welcome to Snake And Ladder Game.");
+			int startPosition=0;
+			System.out.println("The player postion is " + startPosition);
+
+    }
+}
